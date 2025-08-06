051b4157de5bc1aa65754185f4be3309ecdb4ed1
Pipe9177 <andres.charrisp@campusucc.edu.co>
2025-08-06 21:04:54 +0000

    Variable X

--- Clase 1/HolaMundo.java
+++ Clase 1/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 
         System.out.println("Oid chavales, el Hola Mundo en Java!!! Ostias");
 
+        //Creacion de la variable X de tipo entero
+        int x = 2;
     }
 
 
