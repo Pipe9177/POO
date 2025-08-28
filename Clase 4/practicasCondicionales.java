79f2870a5140af33c10b74fedc18bf0681ae3602
Pipe9177 <andres.charrisp@campusucc.edu.co>
2025-08-28 23:30:54 +0000

    Condicionales

---
+++ Clase 4/practicasCondicionales.java
@@ -0,0 +1,17 @@
+public class practicasCondicionales {
+    public static void main(String[] args) {
+        
+   
+    //Ejercio 4.1 A)
+    
+        int angulo = 70;
+
+        
+        if (angulo == 90) {
+            System.out.println("El angulo es un angulo recto");
+        } else {
+            System.out.println("No es un angulo recto");
+        }   
+    }
+}
+    
