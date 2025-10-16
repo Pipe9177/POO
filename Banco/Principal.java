9f2463d43681198947f1d8795f9b9aba71bbfbf3
Pipe9177 <andres.charrisp@campusucc.edu.co>
2025-10-16 00:35:51 +0000

    Banco completado

--- Banco/Principal.java
+++ Banco/Principal.java
@@ -3,7 +3,7 @@ package Banco;
 public class Principal {
     public static void main(String[] args){
         CuentaBancaria cuenta1 = new CuentaBancaria("Yesica Martinez", "123456", 1500.0);
-        System.out.println("Titular: " + cuenta1 + "\n");
+        System.out.println("\nCuenta Bancaria: " + "\n" + cuenta1 + "\n");
         //Ensayo para depositar
         cuenta1.depositar(500.0);
         //Ensayo para retirar
@@ -11,6 +11,6 @@ public class Principal {
         //Ensayo para retirar más de lo que hay en la cuenta
         cuenta1.retirar(2000.0);
         //Se proyecta el final del saldo
-        System.out.println("Saldo final: $" + cuenta1 + "\n");
+        System.out.println("\nInformación Final" + "\n" + cuenta1 + "\n");
     }
 }
