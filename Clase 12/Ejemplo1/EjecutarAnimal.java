abb82a3aebed0ed688bd8a1caadfc907171354b9
Pipe9177 <andres.charrisp@campusucc.edu.co>
2025-10-22 22:35:58 +0000

    Herencia

---
+++ Clase 12/Ejemplo1/EjecutarAnimal.java
@@ -0,0 +1,11 @@
+public class EjecutarAnimal {
+    public static void main(String[] args) {
+        
+        Animal tomoe = new Gato();
+        Animal doki = new Perro();
+
+        tomoe.hacerSonido();
+        doki.hacerSonido();
+
+    }
+}
